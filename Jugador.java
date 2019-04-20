109d5398b4962395d7483e616da57f9c990a6eb2
fjdurlop <fjdurlop0@gmail.com>
2019-04-20 14:22:01 -0500

    clase jugador

---
+++ Jugador.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Jugador{
+	String nombre;
+	//lista de pokemones
+	//lista de pociones
+
+	public void guardarPokemon(){
+		
+	}
+	public void elegirPokemon(){
+		
+	}
+	public void usarPocion(){
+
+	}
+	public void listarPokemon(){
+
+	}
+}
