e89fd1eeb263cd6086e6e5f80c580c364cde8c2d
fjdurlop <fjdurlop0@gmail.com>
2019-04-24 23:01:23 -0500

    modifico metodos de jugador

--- Jugador.java
+++ Jugador.java
@@ -1,22 +1,49 @@
 import java.util.*;
 
 public class Jugador{
-	String nombre;
-	//lista de pokemones
-	//lista de pociones
+	private String nombre;
+	private List<Pokemon> listaPokemones = new ArrayList<Pokemon>();
+	private List<Pocion> listaPociones = new ArrayList<Pocion>();
 
-	//falta constructor
-
-	public void guardarPokemon(){
-		
+	Jugador(String nombre,List<Pokemon> listaPokemones,List<Pocion> listaPociones){
+		this.nombre = nombre;
+		this.listaPokemones = listaPokemones;
+		this.listaPociones = listaPociones;
 	}
-	public void elegirPokemon(){
-		
+
+	public void guardarPokemon(Pokemon poke){
+		int indice= checaIndice(poke.getNombre());
+		listaPokemones.set(indice,poke);
+	}
+	public Pokemon elegirPokemon(Pokemon poke){
+		int indice= checaIndice(poke.getNombre());
+		return listaPokemones.get(indice);
 	}
 	public void usarPocion(){
-
+		//saca pocion
+		//utiliza
+		//eliminar pocion de la lista
 	}
 	public void listarPokemon(){
-
+		for(Pokemon poke : listaPokemones ){
+			System.out.println(poke.getNombre()+"  Vida: "+poke.getVida()+"   Estado: "+poke.getEstado());
+		}
 	}
+
+	public int checaIndice(String nombrePoke){
+		for(int i=0; i<=listaPokemones.size()-1 ; i++){
+			if(nombrePoke.equals(this.nombre)){
+				return i;
+			}
+		}
+	}
+
+	public String getNombre(){
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+	}
+	
 }
